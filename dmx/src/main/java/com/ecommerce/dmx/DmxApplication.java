9231e81a832c34b296bf9c0acfbb3ff1c9870e4b
Adiel <adiel@useswood.com>
2023-10-31 13:40:28 -0300

    first commit

---
+++ dmx/src/main/java/com/ecommerce/dmx/DmxApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.dmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmxApplication.class, args);
+	}
+
+}
---
+++ dmx/src/test/java/com/ecommerce/dmx/DmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.dmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
